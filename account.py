7ca2a831eaa8a8ddbfe24477bcd49383ec31b45b
wanjira-whitney <wanjirawhitney@gmail.com>
2022-06-10 20:21:06 +0300

    current

--- account.py
+++ account.py
@@ -1,6 +1,7 @@
 class Account:
     def __init__(self, name, acc_number):
         self.balance = 0
+        self.transaction = 100
         self.name = name
         self.acc_number = acc_number
         self.deposits = []
@@ -10,15 +11,15 @@ class Account:
             return f"Deposit amount should be greater than zero"
         else:
             self.balance += amount
-            self.deposits.append(f"Hello {self.name}, you have deposited {amount} your balance is {self.balance}")
-            return f"Hello {self.name}, you have deposited {amount}"
+            self.deposits.append(f"Hello {self.name}, you have deposited {amount}")
+            return f"Hello {self.name}, you have deposited {amount} your balance is {self.balance}"
     def withdraw (self, amount):
         if amount > self.balance:
             return f"Your balance is {self.balance}, you cannot withdraw {amount}"
         elif amount <= 0:
             return f"Amount must be greater than zero"
         else:
-            self.balance -= amount
+            self.balance -= amount + self.transaction
             self.withdraws.append(f"You have withdrawn {amount}")
             return f"You have withdrawn {amount}, your new balance is{self.balance}"
     def deposits_statement(self):
@@ -27,4 +28,8 @@ class Account:
     def withdraws_statement(self):
         for staments in self.withdraws:
             print(staments)
+    def current_balance(self):
+        balance = self.balance
+        print(balance)
+
 
